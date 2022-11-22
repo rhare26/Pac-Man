96f590256e19793bf4810648e2caddac40e33037
rhare26 <85655946+rhare26@users.noreply.github.com>
2022-11-22 12:06:11 -0500

    fixed error with update_position in pacman.py

--- pacman.py
+++ pacman.py
@@ -34,7 +34,7 @@ def main():
     while running:
         pressed_keys = pygame.key.get_pressed()
         current_state = current_state.get_next_state(pressed_keys)
-        current_state.update_position()
+        current_state.update(pressed_keys)
         pygame.display.update()  # When done drawing to surface, update screen
         clock.tick(fps)
 
